b2a571e54bcf4acb2879afc2f42979ce177b10eb
Wilmar Nickel <baranguler.65091@gmail.com>
2025-12-23 18:04:46 +0000

    düzeltelim o zaman

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,35 +1,36 @@
-//Scripts for firebase and firebase messaging
-importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js");
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
-// Your web app's Firebase configuration
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// TODO: Replace with your project's customized code snippet
 const firebaseConfig = {
   apiKey: "AIzaSyCtj9XvTW2aiumZhrR3OXQk3U3sh01ffRQ",
   authDomain: "bematch-f168d.firebaseapp.com",
   projectId: "bematch-f168d",
-  storageBucket: "bematch-f168d.appspot.com",
+  storageBucket: "bematch-f168d.firebasestorage.app",
   messagingSenderId: "137528078260",
   appId: "1:137528078260:web:9bbd45115bd2aaf8200ce1",
   measurementId: "G-B9HD1ESD1H"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = firebase.messaging();
 
-
-messaging.onBackgroundMessage(function(payload) {
-  console.log('Received background message ', payload);
-
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.image
+    icon: payload.notification.icon || '/icon-192x192.png'
   };
 
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
