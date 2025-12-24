bd77d1a01bf640158d21788b89bc9ca7358e3fad
Wilmar Nickel <baranguler.65091@gmail.com>
2025-12-24 15:30:24 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBTYHLORHg_HuLzAg74XCanqLE82e92NJI",
-  authDomain: "bematch-new.firebaseapp.com",
-  projectId: "bematch-new",
-  storageBucket: "bematch-new.appspot.com",
-  messagingSenderId: "108504362423",
-  appId: "1:108504362423:web:3781383a547a09590d8c32",
-  measurementId: "G-P6LCQY34GB"
+  apiKey: "AIzaSyCtj9XvTW2aiumZhrR3OXQk3U3sh01ffRQ",
+  authDomain: "bematch-f168d.firebaseapp.com",
+  projectId: "bematch-f168d",
+  storageBucket: "bematch-f168d.appspot.com",
+  messagingSenderId: "137528078260",
+  appId: "1:137528078260:web:9bbd45115bd2aaf8200ce1",
+  measurementId: "G-B9HD1ESD1H"
 };
